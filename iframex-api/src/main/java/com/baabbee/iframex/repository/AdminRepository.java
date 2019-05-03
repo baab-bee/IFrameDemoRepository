2b507a51be02822718060c15084baa12495d37ae
jenifersureshbabu <j.sureshbabu.bee@gmail.com>
2019-05-03 12:26:41 -0400

    First draft of Iframex with moomina souce code and jenifer admin module

---
+++ iframex-api/src/main/java/com/baabbee/iframex/repository/AdminRepository.java
@@ -0,0 +1,17 @@
+package com.baabbee.iframex.repository;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.baabbee.iframex.beans.UserRequest;
+
+public interface AdminRepository extends CrudRepository<UserRequest, String> {
+
+	public List<UserRequest> findByStatus(String status);
+
+	public List<UserRequest> findByStatusIn(Collection<String> statuslist);
+
+}

b3303de8d532608dcb70b4070439641079f2524e
sonia <s.targe.bee@gmail.com>
2019-10-04 13:37:24 -0400

    Removed unused imports

--- iframex-api/src/main/java/com/baabbee/iframex/controller/BeneficiaryRequestController.java
+++ iframex-api/src/main/java/com/baabbee/iframex/controller/BeneficiaryRequestController.java
@@ -1,11 +1,8 @@
 package com.baabbee.iframex.controller;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
--- iframex-api/src/main/java/com/baabbee/iframex/controller/UserController.java
+++ iframex-api/src/main/java/com/baabbee/iframex/controller/UserController.java
@@ -4,11 +4,9 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jackson.JsonObjectDeserializer;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
